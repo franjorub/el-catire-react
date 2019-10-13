2cf90cfe162dcd47fc3fbf632d7d6ce978f72b99
Francisco Rubial <franjorub@gmail.com>
2019-10-13 04:13:45 -0400

    actualizado pantalla de bienvenida

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,15 @@ import React, { Component } from "react";
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>El catire Hot Dog y algo mas...</h2>
+      <div
+        style={{
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <h1>El catire Hot Dog</h1>
+        <h3>Bienvenido</h3>
       </div>
     );
   }
